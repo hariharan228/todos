159c4538f47486534a098274ff9a6199efbfd923
hariharan228 <e.hari.haran.98@gmai.com>
2020-10-22 19:30:47 +0530

    First commit

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -112,8 +112,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/' # the path in url
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-DEBUG = True
-ALLOWED_HOSTS = []
+
+
